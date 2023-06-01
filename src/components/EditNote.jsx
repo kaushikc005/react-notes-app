031bd607d25938fe3e51a83d4fc547e78263dcca
kaushik <kaushikc005@gmail.com>
2023-06-01 15:14:02 +0530

    Added Delete confirmation box

--- src/components/EditNote.jsx
+++ src/components/EditNote.jsx
@@ -1,7 +1,7 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import { MdArrowBackIosNew } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/Ri";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "./EditNote.css";
 import { deleteDoc, doc, setDoc, Timestamp } from "firebase/firestore";
 import { db } from "./utils/firebase";
@@ -9,9 +9,10 @@ import { db } from "./utils/firebase";
 const EditNote = ({ notes, setNotes }) => {
   const { id } = useParams();
   const note = notes.find((item) => item.id === id);
-  console.log(note);
-  const [title, setTitle] = useState(note.title);
-  const [details, setDetails] = useState(note.details);
+  const navigate = useNavigate();
+  const [title, setTitle] = useState(note ? note.title : []);
+  const [details, setDetails] = useState(note ? note.details : []);
+  const [delNote, setDelNote] = useState(false);
 
   const handleSubmit = async () => {
     if (title && details) {
@@ -26,47 +27,80 @@ const EditNote = ({ notes, setNotes }) => {
   };
 
   const handleDelete = async () => {
-    console.log(id);
     if (title && details) {
       const docRef = doc(db, "notes", id);
       await deleteDoc(docRef);
+      navigate("/");
     }
   };
-  return (
-    <article className="editNote--container">
-      <header className="edit--navBar-Header">
-        <nav className="editBtns">
-          <Link to="/">
-            <MdArrowBackIosNew className="edit--note-btn" />
-          </Link>
 
-          {title && details && (
-            <Link to="/" className="edit--note-save-btn" onClick={handleSubmit}>
-              Save
+  return (
+    <>
+      <article className="editNote--container">
+        <header className="edit--navBar-Header">
+          <nav className="editBtns">
+            <Link to="/">
+              <MdArrowBackIosNew className="edit--note-btn" />
             </Link>
-          )}
-          <Link to="/" className="delete-btn" onClick={handleDelete}>
-            <RiDeleteBin6Line />
-          </Link>
-        </nav>
-      </header>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Title"
-          className="edit--note-card--title"
-        />
-        <textarea
-          type="text"
-          value={details}
-          onChange={(e) => setDetails(e.target.value)}
-          placeholder="Enter your note.."
-          className="edit--note-card--details"
-        />
-      </form>
-    </article>
+
+            {title && details && (
+              <Link
+                to="/"
+                className="edit--note-save-btn"
+                onClick={handleSubmit}
+              >
+                Save
+              </Link>
+            )}
+            <RiDeleteBin6Line
+              className="delete-btn"
+              onClick={() => {
+                setDelNote(true);
+              }}
+            />
+          </nav>
+        </header>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            className="edit--note-card--title"
+          />
+          <textarea
+            type="text"
+            value={details}
+            onChange={(e) => setDetails(e.target.value)}
+            placeholder="Enter your note.."
+            className="edit--note-card--details"
+          />
+        </form>
+      </article>
+
+      <aside>
+        <section
+          className="container"
+          style={{ display: delNote ? "flex" : "none" }}
+        >
+          <h3 className="confirmation-text">
+            Do you really want to delete this note?
+          </h3>
+          <nav className="button-container">
+            <button className="confirmation-button" onClick={handleDelete}>
+              Delete
+            </button>
+            <button className="cancel-button" onClick={() => setDelNote(false)}>
+              Cancel
+            </button>
+          </nav>
+        </section>
+        <section
+          className="confirm-bg"
+          style={{ display: delNote ? "flex" : "none" }}
+        ></section>
+      </aside>
+    </>
   );
 };
 
